3811f29d1aa5e1eeb88b8d18c17e0ddf48048e5a
M0H1K4N <nejer911@gmail.com>
2024-03-20 19:33:46 +0400

    manipulating array

--- Training/src/App.jsx
+++ Training/src/App.jsx
@@ -1,21 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 
+const Luka = {
+  name: 'Luka',
+  age: 21,
+  favouriteCar: "PorsChe 911 GT3 RS"
+}
+
 function App() {
-  const [plus, setPlus] = useState(0)
-  const handlePlus = () =>{
-    setPlus(plus + 1)
-  }
-  const handleMinus = () =>{
-    setPlus(plus - 1)
-  }
+
 
   return (
     <>
       <div>
-        <button onClick={handlePlus}>მომატება</button>
-          <h1>{plus}</h1>
-        <button onClick={handleMinus} >მოკლება</button>
+      <h1>My favourite car is: {Luka.favouriteCar}</h1>
      </div>   
     </>
   )
