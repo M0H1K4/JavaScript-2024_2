f3ef4ca550287c46597af1f443624fa0ad8e942e
M0H1K4N <nejer911@gmail.com>
2024-03-21 11:50:08 +0400

    add

--- Training/src/App.jsx
+++ Training/src/App.jsx
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function Changed() {
-  return <h1>This is Changed, good job lion</h1>;
-}
+// function Changed() {
+//   return <h1>This is Changed, good job lion</h1>;
+// }
 
 function App() {
-  const [isClicked, setIsClicked] = useState(true);
-
-  const handleClick = () => {
-    setIsClicked(!isClicked);
+  const [isClicked, setIsClicked] = useState(0);
+  const toggled = () => {
+    setIsClicked(isClicked + 1);
+  };
+  const toggled2 = () => {
+    setIsClicked(isClicked - 1);
   };
 
   return (
     <>
       <div>
-        {isClicked ? (
-          <Changed />
-        ) : (
-          <button onClick={handleClick}>Click to Change</button>
-        )}
+        <button onClick={toggled}>მომატება</button>
+        <h1>{isClicked}</h1>
+        <button onClick={toggled2}>მოკლება</button>
       </div>
     </>
   );
